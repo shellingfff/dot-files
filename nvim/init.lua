7c6b92d438561be6b077b50b218495cff75f1c9c
linux <kaxiford@gmail.com>
2026-09-25 03:54:38 -0400

    Simplify

--- nvim/init.lua
+++ nvim/init.lua
@@ -87,7 +87,22 @@ require("lazy").setup({
 		-- "saghen/blink.cmp",
 		"stevearc/conform.nvim",
 		"nvim-mini/mini.nvim",
-		"davidgranstrom/nvim-markdown-preview",
+                {
+                        "iamcco/markdown-preview.nvim",
+                        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+                        ft = { "markdown" },
+                        build = function()
+                                vim.fn["mkdp#util#install"]()
+                        end,
+                        init = function()
+                                vim.g.mkdp_auto_start = 0
+                                vim.g.mkdp_port = "8080"
+                                vim.g.mkdp_filetypes = { "markdown" }
+                                --vim.g.mkdp_browser = "false"
+                                --vim.g.mkdp_open_to_the_world = 1
+                                --vim.g.mkdp_echo_preview_url = 1
+                        end,
+                },
                 {
                     "shellingfff/follow-md-links.nvim",
                     opts = {
